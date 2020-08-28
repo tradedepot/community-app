c5ab86b2c3929f1af30fb33548ccd1b02601b47b
Uchenna <uokafor@c2gconsulting.com>
2020-08-28 20:39:29 +0100

    add docker compose

---
+++ tagandpush.sh
@@ -0,0 +1,21 @@
+# Initialize tag with circle build num
+if [ ! $CI ]; then
+	tag="localbuild"
+else
+	tag="$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM"
+fi
+
+# Loop through all td images, tag and push
+for i in `docker ps -aq` ; do
+	image=`docker inspect --format="{{ .Config.Image }}" $i`
+	imageid=`docker inspect --format="{{ .Image }}" $i`
+	echo Image is $image
+	if [[ $image == project_fineract-server* ]]; then
+		echo Tagging $image...
+		echo docker tag $imageid tradedepot/banking-web:$tag
+		docker tag $imageid tradedepot/banking-web:$tag
+		if [ $CI ]; then
+			docker push tradedepot/banking:$tag
+		fi
+	fi
+done
